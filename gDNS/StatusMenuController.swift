56563a3fc8c4952a0ebcb1d038ad769f30265868
Louis DeLosSantos <ldelossa.ld@gmail.com>
2017-06-18 23:19:33 -0400

    Status bar menu working

---
+++ gDNS/StatusMenuController.swift
@@ -0,0 +1,21 @@
+//
+//  StatusMenuController.swift
+//  gDNS
+//
+//  Created by louis on 6/18/17.
+//  Copyright © 2017 ldelossa. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenuController: NSObject {
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        let statusItemButton = statusItem.button!
+        statusItemButton.title = "gDNS"
+        
+        statusItem.menu = self.statusMenu
+    }
+}
