c3cc949e6720a5b8ca8eeaea7f3fb47bf980c4f9
Louis DeLosSantos <ldelossa.ld@gmail.com>
2017-06-18 23:11:09 -0400

    Initial Commit

---
+++ gDNS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gDNS
+//
+//  Created by louis on 6/18/17.
+//  Copyright © 2017 ldelossa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
